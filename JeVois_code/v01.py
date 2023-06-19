d5a59277845fd1147e8488a3da5542df8a407a8a
lbhofman <lhofman99@gmail.com>
2023-06-19 15:43:52 +0200

    Added data analysis tool

--- JeVois_code/v01.py
+++ JeVois_code/v01.py
@@ -1,4 +1,3 @@
-# import libjevois as jevois
 import cv2
 import numpy as np
 import random
@@ -6,10 +5,16 @@ import string
 import os
 
 class Test_v1:
-    def __init__(self):
-        # self.timer = jevois.Timer("processing timer", 100, jevois.LOG_INFO)
+    def __init__(self, save_img):
+        try:
+            import libjevois as jevois
+            self.timer = jevois.Timer("processing timer", 100, jevois.LOG_INFO)
+            self.on_jevois = True
+        except:
+            self.on_jevois = False
         self.frame = 0
         self.output_dir = None
+        self.save_img = save_img
 
     def create_output_dir(self):
         base_dir = 'data'
@@ -27,9 +32,13 @@ class Test_v1:
             else:
                 folder_number += 1
 
-    def process(self, inframe, outframe):
-        bgr = inframe.getCvBGR()
-        self.timer.start()
+    def process(self, inframe, outframe, cinframe):
+        if self.on_jevois:
+            bgr = inframe.getCvBGR()
+            self.timer.start()
+        else:
+            bgr = cinframe
+
 
         # Convert the image to YUV color space
         yuv = cv2.cvtColor(bgr, cv2.COLOR_BGR2YUV)
@@ -44,12 +53,13 @@ class Test_v1:
         # Find contours of the orange in the binary mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # Create a copy of the original image for drawing
+        output_image = bgr.copy()
+
         if len(contours) > 0:
             # Find the largest contour
             largest_contour = max(contours, key=cv2.contourArea)
 
-            # Create a copy of the original image for drawing
-            output_image = bgr.copy()
 
             # Draw bounding rectangle around the largest contour and add centroid dot
             x, y, w, h = cv2.boundingRect(largest_contour)
@@ -63,6 +73,12 @@ class Test_v1:
                 # Draw a dot at the centroid
                 cv2.circle(output_image, (cX, cY), 3, (0, 0, 255), -1)
 
+
+
+
+
+
+        if self.on_jevois:
             if self.output_dir is None:
                 self.create_output_dir()
 
@@ -73,14 +89,18 @@ class Test_v1:
             fps = self.timer.stop()
             height, width, _ = output_image.shape
             cv2.putText(output_image, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-
             outframe.sendCv(output_image)
         else:
-            outframe.sendCv(bgr)
+            return output_image, [1, 1, 1]
 
-    def processNoUSB(self, inframe, outframe):
-        bgr = inframe.getCvBGR()
-        self.timer.start()
+    def processNoUSB(self, inframe, outframe, cinframe):
+        if self.on_jevois:
+            bgr = inframe.getCvBGR()
+            self.timer.start()
+        else:
+            bgr = cinframe
+
+
 
         # Convert the image to YUV color space
         yuv = cv2.cvtColor(bgr, cv2.COLOR_BGR2YUV)
@@ -95,16 +115,16 @@ class Test_v1:
         # Find contours of the orange in the binary mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # Create a copy of the original image for drawing
+        output_image = bgr.copy()
+
         if len(contours) > 0:
             # Find the largest contour
             largest_contour = max(contours, key=cv2.contourArea)
 
-            # Create a copy of the original image for drawing
-            output_image = bgr.copy()
-
             # Draw bounding rectangle around the largest contour and add centroid dot
             x, y, w, h = cv2.boundingRect(largest_contour)
-            # cv2.rectangle(output_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(output_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # Calculate the centroid of the largest contour
             M = cv2.moments(largest_contour)
@@ -112,15 +132,21 @@ class Test_v1:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
                 # Draw a dot at the centroid
-                # cv2.circle(output_image, (cX, cY), 3, (0, 0, 255), -1)
+                cv2.circle(output_image, (cX, cY), 3, (0, 0, 255), -1)
 
-        if self.output_dir is None:
-            self.create_output_dir()
 
-        # Save the processed image
-        cv2.imwrite(f'{self.output_dir}/img{self.frame}.png', output_image)
-        self.frame += 1
 
-        fps = self.timer.stop()
-        height, width, _ = output_image.shape
-            # cv2.putText(output_image, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+
+        if not self.on_jevois:
+            return output_image, [1, 1, 1]
+        else:
+            if self.output_dir is None:
+                self.create_output_dir()
+            # Save the processed image
+            cv2.imwrite(f'{self.output_dir}/img{self.frame}.png', output_image)
+            self.frame += 1
+
+            fps = self.timer.stop()
+            height, width, _ = output_image.shape
+            cv2.putText(output_image, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+
